b5ac19ed151b563d975f3b7ebed86020e0972452
sussie <sussie@sussiedeMacBook-Pro.local>
2017-01-22 21:54:03 +0800

    haha

--- itemcf.py
+++ itemcf.py
@@ -1,168 +1,156 @@
-
 "item_based collaborative filtering"
 import sys, random, math
 from operator import itemgetter
-
-
-random.seed(0)
+from math import sqrt
 
 
 class ItemBasedCF():
-    ''' TopN recommendation - ItemBasedCF '''
+    '''  recommendation - ItemBasedCF '''
     def __init__(self):
         self.trainset = {}
         self.testset = {}
+        self.trans_prefs={}
 
         self.n_sim_movie = 20
-        self.n_rec_movie = 10
 
-        self.movie_sim_mat = {}
-        self.movie_popular = {}
-        self.movie_count = 0
+        self.sim_mat = {}
+        #self.movie_count = 0
 
         print >> sys.stderr, 'Similar movie number = %d' % self.n_sim_movie
-        print >> sys.stderr, 'Recommended movie number = %d' % self.n_rec_movie
 
 
-    @staticmethod
-    def loadfile(filename):
-        ''' load a file, return a generator. '''
-        fp = open(filename, 'r')
-        for i, line in enumerate(fp):
-            yield line.strip('\r\n')
-            if i % 100000 == 0:
-                print >> sys.stderr, 'loading %s(%s)' % (filename, i)
-        fp.close()
-        print >> sys.stderr, 'load %s succ' % filename
-
-
-    def generate_dataset(self, filename, pivot=0.7):
-        ''' load rating data and split it to training set and test set '''
+    '''load the trainset'''
+    def load_train_data(self,filename):
+        fp=open(filename,'rb')
+        line=fp.readline()
+        #line=fp.readline()
         trainset_len = 0
-        testset_len = 0
+        while line != '':
+            line.strip('\r\n')
+            user,item,rating,time=line.split(',')
+            self.trainset.setdefault(user, {})
+            self.trainset[user][item] = float(rating)
+            trainset_len += 1
+            line=fp.readline()
 
-        for line in self.loadfile(filename):
-            user, movie, rating, _ = line.split('::')
-            # split the data by pivot
-            if (random.random() < pivot):
-                self.trainset.setdefault(user, {})
-                self.trainset[user][movie] = int(rating)
-                trainset_len += 1
-            else:
-                self.testset.setdefault(user, {})
-                self.testset[user][movie] = int(rating)
-                testset_len += 1
-
-        print >> sys.stderr, 'split training set and test set succ'
+        fp.close()
         print >> sys.stderr, 'train set = %s' % trainset_len
+
+
+
+    '''load the testset'''
+    def load_test_data(self,filename):
+        fp = open(filename, 'rb')
+        #line = fp.readline()
+        line = fp.readline()
+        testset_len = 0
+        while line != '':
+            user,item,rating,time=line.split(',')
+            self.testset.setdefault(user, {})
+            self.testset[user][item] = float(rating)
+            testset_len += 1
+            line=fp.readline()
+
+        fp.close()
         print >> sys.stderr, 'test set = %s' % testset_len
 
+    '''transform the prefs'''
 
-    def calc_movie_sim(self):
-        ''' calculate movie similarity matrix '''
-        print >> sys.stderr, 'counting movies number and popularity...'
+    def transform(self):
+        result = {}
+        for user in self.trainset:
+            for item in self.trainset[user]:
+                result.setdefault(item, {})
+                result[item][user] = self.trainset[user][item]
 
-        for user, movies in self.trainset.iteritems():
-            for movie in movies:
-                # count item popularity 
-                if movie not in self.movie_popular:
-                    self.movie_popular[movie] = 0
-                self.movie_popular[movie] += 1
+        print "finish transform the prefs"
 
-        print >> sys.stderr, 'count movies number and popularity succ'
-
-        # save the total number of movies
-        self.movie_count = len(self.movie_popular)
-        print >> sys.stderr, 'total movie number = %d' % self.movie_count
-
-        # count co-rated users between items
-        itemsim_mat = self.movie_sim_mat
-        print >> sys.stderr, 'building co-rated users matrix...'
-
-        for user, movies in self.trainset.iteritems():
-            for m1 in movies:
-                for m2 in movies:
-                    if m1 == m2: continue
-                    itemsim_mat.setdefault(m1,{})
-                    itemsim_mat[m1].setdefault(m2,0)
-                    itemsim_mat[m1][m2] += 1
-
-        print >> sys.stderr, 'build co-rated users matrix succ'
-
-        # calculate similarity matrix 
-        print >> sys.stderr, 'calculating movie similarity matrix...'
-        simfactor_count = 0
-        PRINT_STEP = 2000000
-
-        for m1, related_movies in itemsim_mat.iteritems():
-            for m2, count in related_movies.iteritems():
-                itemsim_mat[m1][m2] = count / math.sqrt(
-                        self.movie_popular[m1] * self.movie_popular[m2])
-                simfactor_count += 1
-                if simfactor_count % PRINT_STEP == 0:
-                    print >> sys.stderr, 'calculating movie similarity factor(%d)' % simfactor_count
-
-        print >> sys.stderr, 'calculate movie similarity matrix(similarity factor) succ'
-        print >> sys.stderr, 'Total similarity factor number = %d' %simfactor_count
+        return result
 
 
-    def recommend(self, user):
-        ''' Find K similar movies and recommend N movies. '''
-        K = self.n_sim_movie
-        N = self.n_rec_movie
-        rank = {}
-        watched_movies = self.trainset[user]
+    '''calculate the similarity of the two item'''
+    def cos_sim(self,item1,item2):
+        si={}
+        for user in self.trans_prefs[item1]:
+            if user in self.trans_prefs[item2]:si[user]=1
 
-        for movie, rating in watched_movies.iteritems():
-            for related_movie, w in sorted(self.movie_sim_mat[movie].items(),
-                    key=itemgetter(1), reverse=True)[:K]:
-                if related_movie in watched_movies:
+        if len(si)==0:return 0.0
+
+        sum1sq=sum([pow(self.trans_prefs[item1][user],2) for user in si])
+        sum2sq=sum([pow(self.trans_prefs[item2][user],2) for user in si])
+        sum1=sqrt(sum1sq)
+        sum2=sqrt(sum2sq)
+
+        sum_of_item=sum([self.trans_prefs[item1][user]*self.trans_prefs[item2][user] for user in si])
+
+        return sum_of_item/float(sum1*sum2)
+
+
+
+    '''get the simlarity matrix of item'''
+    def get_sim_matrix(self):
+        sim_matrix={}
+        self.trans_prefs=self.transform()
+
+        for item in self.trans_prefs:
+            sim_matrix.setdefault(item, {})
+            for other in self.trans_prefs:
+                if other==item:continue
+                sim_matrix[item][other]=self.cos_sim(item,other)
+
+            #sort the similarity
+            self.sim_mat[item]=sorted(sim_matrix[item].iteritems(),key=lambda b:b[1],reverse=True)
+
+        print "have finished calculate the similarity matrix"
+
+
+
+    '''predict the ratings'''
+    def predict_rating(self,user,item):
+        r=0.0
+        totalsim=0.0
+
+        for i in range(self.n_sim_movie):
+            other=self.sim_mat[item][i][0]
+            if other in self.trainset[user].keys():
+                r=r+self.trainset[user][other]*self.sim_mat[item][i][1]
+
+        '''cannot predict the rating if totalsim equals 0.0, return 0.0 '''
+        if totalsim==0.0: return 0.0
+
+        return r/totalsim
+
+
+
+    '''evaluate the performance with the RMSE'''
+    def rmse(self):
+        n=0.0
+        sum_r=0.0
+        total=0.0
+
+        for user in self.testset.keys():
+            for item in self.testset[user].keys():
+                total=total+1
+
+                if item not in self.sim_mat.keys():
                     continue
-                rank.setdefault(related_movie, 0)
-                rank[related_movie] += w * rating
-        # return the N best movies
-        return sorted(rank.items(), key=itemgetter(1), reverse=True)[:N]
+                rating=self.predict_rating(user,item)
 
+                if rating!=0.0:
+                    n=n+1
+                    sum_r=pow(self.testset[user][item]-rating,2)
+        print "the coverage is ",n/total
+        print "the rmse is",sqrt(sum_r/n)
 
-    def evaluate(self):
-        ''' return precision, recall, coverage and popularity '''
-        print >> sys.stderr, 'Evaluation start...'
-
-        N = self.n_rec_movie
-        #  varables for precision and recall 
-        hit = 0
-        rec_count = 0
-        test_count = 0
-        # varables for coverage
-        all_rec_movies = set()
-        # varables for popularity
-        popular_sum = 0
-
-        for i, user in enumerate(self.trainset):
-            if i % 500 == 0:
-                print >> sys.stderr, 'recommended for %d users' % i
-            test_movies = self.testset.get(user, {})
-            rec_movies = self.recommend(user)
-            for movie, w in rec_movies:
-                if movie in test_movies:
-                    hit += 1
-                all_rec_movies.add(movie)
-                popular_sum += math.log(1 + self.movie_popular[movie])
-            rec_count += N
-            test_count += len(test_movies)
-
-        precision = hit / (1.0 * rec_count)
-        recall = hit / (1.0 * test_count)
-        coverage = len(all_rec_movies) / (1.0 * self.movie_count)
-        popularity = popular_sum / (1.0 * rec_count)
-
-        print >> sys.stderr, 'precision=%.4f\trecall=%.4f\tcoverage=%.4f\tpopularity=%.4f' \
-                % (precision, recall, coverage, popularity)
 
 
 if __name__ == '__main__':
-    ratingfile = 'ml-1m/ratings.dat'
+    trainfile = r'test1/train_rating.txt'
+    testfile  = r'test1/train_rating.txt'
     itemcf = ItemBasedCF()
-    itemcf.generate_dataset(ratingfile)
-    itemcf.calc_movie_sim()
-    itemcf.evaluate()
+    itemcf.load_train_data(trainfile)
+    itemcf.load_test_data(testfile)
+    itemcf.get_sim_matrix()
+    itemcf.rmse()
+
+
